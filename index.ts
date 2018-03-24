163bddb9d64589f3fdb49c6a81efa406a620aa5b
Magomed Dadagov <grozniy.info@gmail.com>
2018-03-24 18:01:20 +0300

    select range dates watch dates

--- index.ts
+++ index.ts
@@ -1,42 +1,90 @@
 class McDatesController implements angular.IComponentController {
     [x: string]: any;
-
-    change = () => {
-        if (this.ctrl.mcChange !== undefined)
-            this.ctrl.mcChange()
-    }
     private FORMAT = "YYYY-MM-DD";
+    private _from: Date;
+    private _to: Date;
 
-    constructor() {
-        this.ctrl = this;
-        this._from = moment().toDate();
-        this._to = moment().toDate()
+    constructor(private $scope: angular.IScope) {
+
+        $scope.$watch(() => this.dateFrom, (newValue, oldValue) => {
+            let m = moment(newValue, this.FORMAT);
+            if (m.format(this.FORMAT) === newValue) {
+                this.from = m.toDate()
+            }
+        });
+
+        $scope.$watch(() => this.dateTo, (newValue, oldValue) => {
+            let m = moment(newValue, this.FORMAT);
+            if (m.format(this.FORMAT) === newValue) {
+                this.to = m.toDate()
+            }
+        })
     }
-
-    private _from;
 
     get from() {
         return this._from
     }
 
     set from(value) {
+        if (value == null) {
+            this._from = undefined;
+            this.dateFrom = value;
+            return
+        }
         let m = moment(value, this.FORMAT);
         this._from = m.toDate();
-        this.ctrl.dateFrom = m.format(this.FORMAT)
+        this.dateFrom = m.format(this.FORMAT)
 
     }
 
-    private _to;
 
     get to() {
         return this._to
     }
 
     set to(value) {
+        if (value == null) {
+            this._to = undefined;
+            this.dateTo = value;
+            return
+        }
         let m = moment(value, this.FORMAT);
         this._to = m.toDate();
-        this.ctrl.dateTo = m.format(this.FORMAT)
+        this.dateTo = m.format(this.FORMAT)
     }
+
+    change() {
+        if (this.mcChange !== undefined)
+            this.mcChange()
+    }
+
+    setDate(value) {
+
+        switch (value) {
+            case "yesterday":
+                this.from = moment().subtract(1, "days").toDate();
+                this.to = moment().subtract(1, "days").toDate();
+                break;
+            case "today":
+                this.from = moment().toDate();
+                this.to = moment().toDate();
+                break;
+            case "two_weeks":
+                this.from = moment().subtract(2, "weeks").toDate();
+                this.to = moment().toDate();
+                break;
+            case "month":
+                this.from = moment().subtract(1, "months").toDate();
+                this.to = moment().toDate();
+                break;
+            case "all":
+                this.from = null;
+                this.to = null;
+                break
+        }
+        this.change()
+    }
+
 }
 
 
@@ -63,11 +111,11 @@ class McDatesComponent implements angular.IComponentOptions {
                  
           </div>
           <div>
-           <a ng-click="$ctrl.change()" href="">Вчера</a>
-           <a ng-click="$ctrl.change()" href="">Сегодня</a>
-           <a ng-click="$ctrl.change()" href="">2 недели</a>
-           <a ng-click="$ctrl.change()" href="">Месяц</a>
-           <a ng-click="$ctrl.change()" href="">Все</a>
+           <a ng-click="$ctrl.setDate('yesterday')" href="">Вчера</a>
+           <a ng-click="$ctrl.setDate('today')" href="">Сегодня</a>
+           <a ng-click="$ctrl.setDate('two_weeks')" href="">2 недели</a>
+           <a ng-click="$ctrl.setDate('month')" href="">Месяц</a>
+           <a ng-click="$ctrl.setDate('all')" href="">Все</a>
           </div>
       
         `;
